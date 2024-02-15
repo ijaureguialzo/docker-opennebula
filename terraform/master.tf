46e0f6d74991131338fceba7d296636558243301
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-02-15 03:33:44 +0100

    Mostrar la salida a color de Ansible

--- terraform/master.tf
+++ terraform/master.tf
@@ -48,6 +48,7 @@ resource "null_resource" "ansible_master" {
   provisioner "local-exec" {
     command = <<EOT
       ANSIBLE_HOST_KEY_CHECKING=False \
+      ANSIBLE_FORCE_COLOR=True \
       ansible-playbook \
         -i "${local.master.connection_ip}," \
         /ansible/master-playbook.yml
